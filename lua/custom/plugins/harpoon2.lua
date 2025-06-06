a387f461fd0c469aac92a4fc912763c64a32a70d
marmastr <marmastr@gmail.com>
2025-06-05 22:42:26 -0400

    done for a while

---
+++ lua/custom/plugins/harpoon2.lua
@@ -0,0 +1,110 @@
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          '<leader>H',
+          function()
+            require('harpoon'):list():add()
+          end,
+          desc = 'Harpoon File',
+        },
+        {
+          '<leader>h',
+          function()
+            local harpoon = require 'harpoon'
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = 'Harpoon Quick Menu',
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          -- '<leader>' .. i,
+          '' .. i,
+          function()
+            require('harpoon'):list():select(i)
+          end,
+          desc = 'Harpoon to File ' .. i,
+        })
+      end
+      return keys
+    end,
+  },
+  -- {
+  --   'ThePrimeagen/harpoon',
+  --   branch = 'harpoon2',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'nvim-telescope/telescope.nvim',
+  --   },
+  --   config = function()
+  --     require('harpoon'):setup()
+  --   end,
+  --   keys = {
+  --     {
+  --       '<leader>a',
+  --       function()
+  --         require('harpoon'):list():add()
+  --       end,
+  --       desc = '[a]dd file to harpoon',
+  --     },
+  --     {
+  --       '<leader>A',
+  --       function()
+  --         local harpoon = require 'harpoon'
+  --         harpoon.ui:toggle_quick_menu(harpoon:list())
+  --       end,
+  --       desc = 'harpoon quick m[e]nu',
+  --     },
+  --     {
+  --       '<leader>1',
+  --       function()
+  --         require('harpoon'):list():select(1)
+  --       end,
+  --     },
+  --     {
+  --       '<leader>2',
+  --       function()
+  --         require('harpoon'):list():select(2)
+  --       end,
+  --     },
+  --     {
+  --       '<leader>3',
+  --       function()
+  --         require('harpoon'):list():select(3)
+  --       end,
+  --     },
+  --     {
+  --       '<leader>4',
+  --       function()
+  --         require('harpoon'):list():select(4)
+  --       end,
+  --     },
+  --     -- Toggle previous & next buffers stored within require("harpoon") list
+  --     {
+  --       '<C-S-P>',
+  --       function()
+  --         require('harpoon'):list():prev()
+  --       end,
+  --     },
+  --     {
+  --       '<C-S-N>',
+  --       function()
+  --         require('harpoon'):list():next()
+  --       end,
+  --     },
+  --   },
+  -- },
+}
--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -1,6 +1,7 @@
 return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
+    version = '*',
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -16,16 +17,78 @@ return {
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
+      require('mini.tabline').setup()
+      require('mini.move').setup()
+      require('mini.files').setup {
+        -- Customization of shown content
+        content = {
+          -- Predicate for which file system entries to show
+          filter = nil,
+          -- What prefix to show to the left of file system entry
+          prefix = nil,
+          -- In which order to show file system entries
+          sort = nil,
+        },
 
+        -- Module mappings created only inside explorer.
+        -- Use `''` (empty string) to not create one.
+        mappings = {
+          close = 'q',
+          go_in = 'l',
+          go_in_plus = 'L',
+          go_out = 'h',
+          go_out_plus = 'H',
+          mark_goto = "'",
+          mark_set = 'm',
+          reset = '<BS>',
+          reveal_cwd = '@',
+          show_help = 'g?',
+          synchronize = '=',
+          trim_left = '<',
+          trim_right = '>',
+        },
+
+        -- General options
+        options = {
+          -- Whether to delete permanently or move into module-specific trash
+          permanent_delete = true,
+          -- Whether to use for editing directories
+          use_as_default_explorer = true,
+        },
+
+        -- Customization of explorer windows
+        windows = {
+          -- Maximum number of windows to show side by side
+          max_number = math.huge,
+          -- Whether to show preview of file/directory under cursor
+          preview = false,
+          -- Width of focused window
+          width_focus = 50,
+          -- Width of non-focused window
+          width_nofocus = 15,
+          -- Width of preview window
+          width_preview = 25,
+        },
+      }
+      local minifiles_toggle = function(...)
+        if not MiniFiles.close() then
+          MiniFiles.open(...)
+        end
+      end
+      vim.keymap.set('n', '\\', function()
+        minifiles_toggle()
+      end, { desc = 'mini.files toggle' })
+      -- require('mini.hipatterns').setup()
+      require('mini.icons').setup()
+      require('mini.git').setup()
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
       statusline.setup { use_icons = vim.g.have_nerd_font }
-
       -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
+
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
@@ -36,5 +99,7 @@ return {
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  -- { 'echasnovski/mini-tabline' },
+  -- { 'echasnovski/mini-hipatterns' },
 }
 -- vim: ts=2 sts=2 sw=2 et
