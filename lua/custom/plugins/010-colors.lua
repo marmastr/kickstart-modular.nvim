452239699550c5a55594e54ccc32f65942d8587c
marmastr <marmastr@gmail.com>
2025-06-13 14:02:05 -0400

    colors were not working. not fond of the solution

--- lua/custom/plugins/010-colors.lua
+++ lua/custom/plugins/010-colors.lua
@@ -1,7 +1,14 @@
+function ColorMyPencils(color)
+  color = color or 'nightfox'
+  vim.cmd.colorscheme(color)
+
+  -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
 return {
   {
     'EdenEast/nightfox.nvim',
-    priority = 1000,
     -- name = 'nightfox',
     config = function()
       require('nightfox').setup {
@@ -46,6 +53,31 @@ return {
         palettes = {},
         specs = {},
         groups = {},
+      }
+      -- setup must be called before loading
+      -- vim.cmd("colorscheme nightfox")
+      ColorMyPencils()
+    end,
+  },
+
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = 'moon', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
       }
     end,
   },
